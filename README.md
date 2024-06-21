9bacbc0e87601a2b9fb7e65b8ecfccfe22a9a107
kagura114 <me@lolicon.cyou>
2024-06-22 00:12:33 +0800

    🎉 find image by pid is now avaliable

--- README.md
+++ README.md
@@ -14,11 +14,51 @@ Clone this repo, then `npx wrangler dev` to build and start a local server, you 
 To deploy on your cloudflare account, change the `name` field, which is the name of your worker, in `wrangler.toml` and then use `npx wrangler deploy` to upload.
 
 ## Usage
+### General
 For every image in pixiv, jpg/png files are accessed under `i.pximg.net`, and need a valid `REFERER` to access data. So we add referer and then proxy our request to the original server.\
 So the only thing users need to do is to change `i.pximg.net` to `your.worker.domain` and everything will go.
 
+### PID API: /pid/{pid}/{page}
+`pid` is the pid of the gallary\
+`page` is the page (e.g. `_p0`) of the gallary\
+As pixiv blocks cloudflare workers from `pixiv.net` and proxying is not easy on wasm, another proxy is needed for getting data from pixiv.\
+To enable this, put `host` and `auth` into your worker's [secret](https://developers.cloudflare.com/workers/cli-wrangler/commands#secret), usually use `wrangler secret put <KEY>` and prompt your secret \
+`host` is the `hostname` of your proxy, e.g. `p.example.com`, do not add scheme\
+`auth` is the `secret token` of your proxy, e.g. `123456`, don't leave blank!\
+Also here is an example of your nginx config
+```
+server {
+	listen [::]:443 ssl;
+	listen 443 ssl;
+
+	server_name <hostname>;
+	resolver 8.8.8.8;	
+
+	ssl_certificate <your cert>;
+	ssl_certificate_key <your key>;
+
+	location / {
+	        proxy_set_header Host www.pixiv.net;
+	        proxy_set_header Referer "https://www.pixiv.net";
+	        proxy_pass https://www.pixiv.net;
+	        proxy_ssl_server_name on;
+	        proxy_set_header Accept-Encoding "";
+		
+            if ($http_authorization != "Bearer <secret token>") {
+                return 401;
+            }
+
+	        sub_filter_once off;
+	        sub_filter_types *;
+		    client_max_body_size 8000m;
+	}
+}
+```
 ## Useful hints
 For more help in reqwest, see [docs.rs/reqwest](https://docs.rs/reqwest/latest/reqwest/)
+
+## Known Problems
+- PID API cannot get image url if the content is R18
 
 ## Issues
 If you have any problems with this, you are welcome to leave an issue.
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 use regex::Regex;
-use reqwest::{header::REFERER, Client, StatusCode};
+use reqwest::{
+    header::{AUTHORIZATION, REFERER},
+    Client, StatusCode,
+};
 use worker::*;
 
 #[event(fetch)]
-async fn main(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     if req.path().starts_with("/pid") {
-        get_image_by_pid(req).await
+        get_image_by_pid(req, env).await
     } else if req.path().starts_with("/img-original") {
         get_image_by_url(req).await
     } else {
@@ -40,20 +43,47 @@ async fn get_image_by_url(req: Request) -> Result<Response> {
     }
 }
 
-async fn get_image_by_pid(req: Request) -> Result<Response> {
-    let pid = match req.path().split('/').last() {
-        Some(p) => p.parse::<i64>().unwrap(),
-        None => {
-            return Response::error(
-                "Failed to parse pid from your request",
-                StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
-            )
+async fn get_image_by_pid(req: Request, env: Env) -> Result<Response> {
+    let mut pid: i64 = -1;
+    let mut page: i64 = 0; // {pid}_p{page}.#ext
+
+    req.path().split('/').for_each({
+        |sp| match sp.parse::<i64>() {
+            Ok(p) => {
+                if pid == -1 {
+                    pid = p
+                } else {
+                    page = p
+                }
+            }
+            Err(_) => (),
+        }
+    });
+
+    if pid == -1 {
+        return Response::error(
+            "Failed to parse pid from your request",
+            StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
+        );
+    }
+
+    let host = match env.secret("host") {
+        Ok(h) => h.to_string(),
+        Err(_) => {
+            return Response::error("Not avaliable.", StatusCode::SERVICE_UNAVAILABLE.as_u16())
         }
     };
-    let client = Client::new();
-    let data = client
-        .get(format!("https://www.pixiv.net/artworks/{pid}"))
-        .header(REFERER, "https://www.pixiv.net/")
+
+    let auth = match env.secret("auth") {
+        Ok(a) => a.to_string(),
+        Err(_) => {
+            return Response::error("Not avaliable.", StatusCode::SERVICE_UNAVAILABLE.as_u16())
+        }
+    };
+
+    let data = Client::new()
+        .get(format!("https://{host}/artworks/{pid}"))
+        .header(AUTHORIZATION, format!("Bearer {auth}"))
         .send()
         .await;
 
@@ -69,16 +99,19 @@ async fn get_image_by_pid(req: Request) -> Result<Response> {
 
     if let Some(captures) = re.captures(response.as_str()) {
         if let Some(url) = captures.get(1) {
-            return Ok(
-                get_image_by_url(Request::new(url.as_str(), Method::Post).unwrap())
-                    .await
-                    .unwrap(),
-            );
+            return Ok(get_image_by_url(
+                Request::new(
+                    url.as_str()
+                        .replace("_p0", format!("_p{page}").as_str())
+                        .as_str(),
+                    Method::Post,
+                )
+                .unwrap(),
+            )
+            .await
+            .unwrap());
         }
     }
 
-    Response::error(
-        format!("Failed to get img source for request\nresponse:\n{response}"),
-        StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
-    )
+    Response::error(response, StatusCode::INTERNAL_SERVER_ERROR.as_u16())
 }
